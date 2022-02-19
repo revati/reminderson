796d866892378835818abb4d6d447cc46338010c
Roberts Gulans <rgulans@gmail.com>
2022-02-19 18:37:00 +0200

    releases with fly io

--- config/runtime.exs
+++ config/runtime.exs
@@ -27,13 +27,13 @@ if config_env() == :prod do
       For example: ecto://USER:PASS@HOST/DATABASE
       """
 
-  maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
+  # maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
 
   config :reminderson, Reminderson.Repo,
     # ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
+    socket_options: [:inet6]
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
@@ -47,11 +47,16 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  app_name =
+    System.get_env("FLY_APP_NAME") ||
+      raise "FLY_APP_NAME not available"
+
+  host = System.get_env("PHX_HOST") || "#{app_name}.fly.dev"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :reminderson, RemindersonWeb.Endpoint,
-    url: [host: host, port: 443],
+    server: true,
+    url: [host: host, port: 80],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
---
+++ rel/overlays/bin/migrate.bat
@@ -0,0 +1,1 @@
+call "%~dp0\reminderson" eval Reminderson.Release.migrate
